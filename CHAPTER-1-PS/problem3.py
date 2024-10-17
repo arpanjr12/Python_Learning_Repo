f28b678dc8d93dab2b29939e4b663561d6cf5f6d
Arpan Das <arpandasjr03@gmail.com>
2024-10-17 10:20:22 +0530

    Update problem3.py

--- CHAPTER-1-PS/problem3.py
+++ CHAPTER-1-PS/problem3.py
@@ -1,5 +1,5 @@
 #Install an external module and use it to perform an operation of your interest
 import pyttsx3
 engine = pyttsx3.init()
-engine.say('kodom gache bose ache kanu haramzada  ')
+engine.say('learning python ')
 engine.runAndWait()
